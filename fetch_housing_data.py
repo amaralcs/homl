7af20a7679f42ce2dab1bfdb3e8cafa74d3d667d
carlos <amaralcscgp@gmail.com>
2019-12-17 20:52:45 +0000

    initial commit

---
+++ fetch_housing_data.py
@@ -0,0 +1,18 @@
+import os
+import tarfile
+import urllib.request
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+	os.makedirs(housing_path, exist_ok=True)
+	tgz_path = os.path.join(housing_path, "housing.tgz")
+	urllib.request.urlretrieve(housing_url, tgz_path)
+	housing_tgz = tarfile.open(tgz_path)
+	housing_tgz.extractall(path=housing_path)
+	housing_tgz.close()
+
+if __name__ == '__main__':
+	fetch_housing_data()
